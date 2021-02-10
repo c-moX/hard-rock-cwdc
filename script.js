b46cb9c7fdc0dae8cbda1dc24d58a42dcbe58cfa
c-moX <cmoxsh@gmail.com>
2021-02-10 15:41:09 +0600

    javaScrpt file complete

--- script.js
+++ script.js
@@ -50,28 +50,34 @@ const displaySongs = songs => {
 
 // lyrics data loaded:
 // Step: 01 OR 02
-const getLyric = (artist, title) => {
-    // console.log(artist, title); // xxx::xxx
-    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
-    // console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayLyrics(data.lyrics))
-        // .catch(error => displayError(error));
-        .catch(error => displayError("Something Went Wrong!! Please Try Again Later!"));
-}
-
-// Step: 02 OR 01
-// const getLyric = async (artist, title) => { // For async && await
+// const getLyric = (artist, title) => {
 //     // console.log(artist, title); // xxx::xxx
 //     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
 //     // console.log(url);
-//     const res = await fetch(url); // For async && await
-//     const data = await res.json();
-//     displayLyrics(data.lyrics);
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => displayLyrics(data.lyrics))
+//         // .catch(error => displayError(error));
+//         .catch(error => displayError("Something Went Wrong!! Please Try Again Later!"));
 // }
 
-// displayed lyrics:
+// Step: 02 OR 01
+const getLyric = async (artist, title) => { // For async && await
+    // console.log(artist, title); // xxx::xxx
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    // console.log(url);
+    try { // try syntax condition
+        const res = await fetch(url); // For async && await
+        const data = await res.json();
+        displayLyrics(data.lyrics);
+    }
+    catch (error) {
+        // displayError(error);
+        displayError("Something Went Wrong!! Please Try Again Later!");
+    }
+}
+
+// displayed lyrics text:
 const displayLyrics = lyrics => {
     const lyricsDiv = document.getElementById("song-lyrics");
     lyricsDiv.innerText = lyrics;
